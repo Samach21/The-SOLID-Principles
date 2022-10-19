03b095bb5c82d8fcbf7d4ec2db3d58457076a6ff
samach21 <samach21@gmail.com>
2022-10-19 21:12:59 +0700

    2nd done

--- SecondProblem/src/BookPublisher.java
+++ SecondProblem/src/BookPublisher.java
@@ -3,6 +3,6 @@ import java.util.List;
 public class BookPublisher {
     public static void main(String[] args) {
         Book book = new Book("Tyland", List.of("I", "moved", "here", "recently", "too"));
-        book.printToFile();
+        book.print(new FilePrinter());
     }
 }
